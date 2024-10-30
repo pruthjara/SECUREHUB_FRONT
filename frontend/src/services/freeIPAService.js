83227ff9fc4ec58513a32862ef4de7b59bb05633
pruthjara <pruthjperez@gmail.com>
2024-10-30 09:35:14 +0100

    Añadir logo dit

---
+++ frontend/src/services/freeIPAService.js
@@ -0,0 +1,10 @@
+// src/services/freeIPAService.js
+export async function getUsers() {
+  const response = await fetch('http://138.4.11.247:9000/api/freeipa/users'); // URL completa del backend
+  if (!response.ok) {
+    throw new Error('Error al obtener usuarios');
+  }
+  const data = await response.json();
+  console.log("Datos recibidos del backend:", data); // Para verificar los datos
+  return data;
+}
