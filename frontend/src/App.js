1e6afbb9c47abdd36f98cada78c4f22ea83ac1df
pruthjara <pruthjperez@gmail.com>
2024-12-15 17:54:19 +0100

    Crear App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import HomePage from './containers/HomePage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Users from "./pages/Users";
+import Groups from "./pages/Groups";
+import User from "./pages/User";
+import Login from './components/Login';
+import "./styles/Global.css";
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/freeipa/allusers" element={<Users />} />
+        <Route path="/freeipa/groups" element={<Groups />} />
+        <Route path="/freeipa/user/:username" element={<User />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
