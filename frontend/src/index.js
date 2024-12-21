839c5e5b5e5e9545f8c2871e4c12c60d0641e894
pruthjara <ruth.jperez@alumnos.upm.es>
2024-12-21 20:56:45 +0100

    empezar de cero la implementación de keycloak

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,22 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ReactKeycloakProvider } from '@react-keycloak/web';
-import Keycloak from 'keycloak-js';
-
-// Configuración de Keycloak
-const keycloak = new Keycloak({
-  url: 'http://keycloak-service:8080/auth', // URL de tu servidor Keycloak
-  realm: 'securehub', // Nombre del realm configurado en Keycloak
-  clientId: 'securehub-frontend', // Client ID configurado en Keycloak para tu frontend
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ReactKeycloakProvider authClient={keycloak}>
-      <App />
-    </ReactKeycloakProvider>
+    <App />
   </React.StrictMode>
 );
 
@@ -26,4 +15,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,40 +1,24 @@
 import axios from 'axios';
-import { keycloak } from '@react-keycloak/web'; // Usamos la instancia Keycloak del contexto
 
 // Base URL del backend
 const API_BASE_URL = 'http://138.4.11.247:9000/freeipa';
 
-// Configuración del cliente Axios con Interceptores
-const apiClient = axios.create({
-  baseURL: API_BASE_URL,
-});
+const BASE_URL = 'http://138.4.11.247:9000';
 
-// Interceptor para agregar el token de Keycloak al encabezado Authorization
-apiClient.interceptors.request.use(
-  async (config) => {
-    // Verifica si Keycloak está autenticado
-    if (keycloak.authenticated) {
-      const token = await keycloak.token;
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-// Métodos para interactuar con el backend
 export const fetchUserDetails = (username) => {
-  return apiClient.get(`/user/${username}`);
+  return axios.get(`${API_BASE_URL}/user/${username}`);
 };
 
 export const fetchAllUsers = () => {
-  return apiClient.get(`/allusers`);
+  return axios.get(`${API_BASE_URL}/allusers`);
 };
 
 export const fetchGroups = () => {
-  return apiClient.get(`/groups`);
+  return axios.get(`${API_BASE_URL}/groups`);
 };
 
+
+export const login = (username, password) => {
+  return axios.post(`${BASE_URL}/login`, { username, password });
+};
 
