18f95de98bea96c99565ee5b7186ea5ee1ace027
pruthjara <ruth.jperez@alumnos.upm.es>
2024-12-22 14:16:43 +0100

    nuevo código implementación keycloak

--- frontend/src/keycloak.js
+++ frontend/src/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-    url: 'http://192.168.100.3:8080/',
+    url: 'http://localhost:8080',
     realm: 'master',
     clientId: 'frontend-client',
 });
