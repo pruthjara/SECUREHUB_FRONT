9b08009f237dedab3ef525ff6e631e7ea114469f
pruthjara <ruth.jperez@alumnos.upm.es>
2025-03-12 14:07:01 +0100

    mejoras front

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = ({ handleLogout }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className="header">
       <div className="header-content">
-        <img src="/dit-logo.png" alt="Logo" className="logo" />
-        <h1>SecureHub</h1>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/freeipa/allusers">Users</Link>
-          <Link to="/freeipa/groups">Groups</Link>
-          <button className="logout-button" onClick={handleLogout}>
-            Cerrar sesión
-          </button>
+        <div className="logo-container">
+          <img src="/dit-logo.png" alt="Logo" className="logo" />
+          <h1>SecureHub</h1>
+        </div>
+
+        {/* Botón de menú hamburguesa para móviles */}
+        <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+          ☰
+        </button>
+
+        <nav className={menuOpen ? "nav open" : "nav"}>
+          <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+          <Link to="/freeipa/allusers" onClick={() => setMenuOpen(false)}>Users</Link>
+          <Link to="/freeipa/groups" onClick={() => setMenuOpen(false)}>Groups</Link>
+          <button className="logout-button" onClick={handleLogout}>Cerrar sesión</button>
         </nav>
       </div>
     </header>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // React 18 utiliza createRoot
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // Importa estilos globales si tienes
+import "./index.css"; 
 
-// Renderizar la aplicación
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
